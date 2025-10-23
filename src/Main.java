5cc00b87a9c13a5e3fd062f1b4b2c86232540fc2
aidarmukhoiarov <https://github.com/tenezma@gmail.com/helloworld.git>
2025-10-23 23:06:32 +0300

    Commit1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+        //1 exercise
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = firstName + " " + middleName + " " + lastName;
+        System.out.println("Ф. И. О. сотрудника - " + fullName);
+        //2 exercise
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета -" + fullName.toUpperCase());
+        //3 exercise
+        String employeeName = "Иванов Семён Семёнович";
+        String newEmployeeName = employeeName.replace('ё', 'е');
+        System.out.println("Данные Ф. И. О. сотрудника — " + newEmployeeName);
 
     }
 }
